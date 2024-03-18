80e496c062b7c28e1a795120e69aed2156a1b356
steel <gawct.231020@gmail.com>
2024-03-18 14:30:55 +0900

    完成

--- Assets/Scripts/CandyDestroyer.cs
+++ Assets/Scripts/CandyDestroyer.cs
@@ -6,12 +6,23 @@ public class CandyDestroyer : MonoBehaviour
 {
     public CandyManager candyManager;
     public int reward;
+    public GameObject effectPrefab;
+    public Vector3 effectRotation;
     void OnTriggerEnter(Collider other){
         if(other.gameObject.tag=="Candy"){
             //指定数だけCandyのストックを増やす
             candyManager.AddCandy(reward);
             //オブジェクトの削除
             Destroy(other.gameObject);
+
+            if(effectPrefab!=null){
+                //Candyのポジションにエフェクトを作成
+                Instantiate(
+                    effectPrefab,
+                    other.transform.position,
+                    Quaternion.Euler(effectRotation)
+                );
+            }
         }
     }
     // Start is called before the first frame update
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,6 +7,7 @@ public class Shooter : MonoBehaviour
     const int MaxShotPower=5;
     const int RecoverySeconds=3;
     int shotPower=MaxShotPower;
+    AudioSource shotSound;
     public GameObject[] candyPrefabs;
     public Transform candyParentTransform;
     public CandyManager candyManager;
@@ -16,7 +17,7 @@ public class Shooter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotSound=GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -54,6 +55,8 @@ public class Shooter : MonoBehaviour
         candyManager.ConsumeCandy();
         //ShotPowerを消費
         ConsumePower();
+        //サウンドを再生
+        shotSound.Play();
     }
     void OnGUI(){
         GUI.color=Color.black;
