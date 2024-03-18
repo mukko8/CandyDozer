aceb48c3490bd873f10820236958a509c4b575ad
steel <gawct.231020@gmail.com>
2024-03-18 11:14:46 +0900

    P205

---
+++ Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour
+{
+    public CandyManager candyManager;
+    public int reward;
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag=="Candy"){
+            //指定数だけCandyのストックを増やす
+            candyManager.AddCandy(reward);
+            //オブジェクトの削除
+            Destroy(other.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    public GameObject candyPrefab;
+    public GameObject[] candyPrefabs;
+    public Transform candyParentTransform;
+    public CandyManager candyManager;
     public float shotForce;
     public float shotTorque; //回転力
+    public float baseWidth;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +21,32 @@ public class Shooter : MonoBehaviour
     {
         if(Input.GetButtonDown("Fire1")) Shot(); //LCtrlorClick       
     }
+    //キャンディのプレハブからランダムに一つ選ぶ
+    GameObject SampleCandy(){
+        int index=Random.Range(0,candyPrefabs.Length);
+        return candyPrefabs[index];
+    }
+    Vector3 GetInstantiatePosition(){
+        //画面のサイズとInputの割合からキャンディ生成のポジションを計算
+        float x=baseWidth*(Input.mousePosition.x/Screen.width)-(baseWidth/2);
+        return transform.position+new Vector3(x,0,0);
+    }
     public void Shot(){
+        //キャンディを生成できる条件外ならShotしない
+        if(candyManager.GetCandyAmount()<=0)return;
         //プレハブからCandyオブジェクトを生成
-        GameObject candy=Instantiate(
-            candyPrefab,
-            transform.position,
+        GameObject candy=(GameObject)Instantiate(
+            SampleCandy(),
+            GetInstantiatePosition(),
             Quaternion.identity
             );
+        //生成したCandyオブジェクトの親をcandyParentTransformに設定する
+        candy.transform.parent=candyParentTransform;
         //CandyオブジェクトのRigidbodyを取得し力と回転を加える
         Rigidbody candyRigidBody=candy.GetComponent<Rigidbody>();
         candyRigidBody.AddForce(transform.forward*shotForce);
         candyRigidBody.AddTorque(new Vector3(0,shotTorque,0));
+        //candyのストックを消費
+        candyManager.ConsumeCandy();
     }
 }
